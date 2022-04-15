b6bbb3a67149472bcf4857daa366ae270fee9a4d
jxrvll <jxrvll@gmail.com>
2022-04-14 21:27:39 -0400

    Fix image issue

--- components/image.js
+++ components/image.js
@@ -4,14 +4,14 @@ import NextImage from "next/image"
 const Image = ({ image, style }) => {
   const { url, alternativeText, width, height } = image.data.attributes
 
-  // const loader = () => {
-  //   return getStrapiMedia(image)
-  // }
+  const loader = () => {
+    return getStrapiMedia(image)
+  }
 
   return (
     <>
     <NextImage
-      // loader={loader}
+      loader={loader}
       layout="responsive"
       width={width}
       height={height}
